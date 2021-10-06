32459921b154955adfda7b54d39876bb59d5e225
Xan B <1634627+xan00b@users.noreply.github.com>
2021-10-06 09:19:27 -0700

    change codon table/dict code to not use index numbers

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -9,6 +9,7 @@ import argparse
 
 
 # --------------------------------------------------
+
 def get_args():
     """Get command-line arguments"""
 
@@ -16,35 +17,24 @@ def get_args():
         description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('seq',
                         metavar='str',
-                        help='A positional argument')
+                        help='DNA or RNA sequence')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
+    parser.add_argument('-c',
+                        '--codons',
                         help='A readable file',
                         metavar='FILE',
                         type=argparse.FileType('rt'),
-                        default=None)
+                        default=None,
+                        required=True)
 
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--outfile',
+                        help='A writable file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default='out.txt')
 
     return parser.parse_args()
 
@@ -54,17 +44,22 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    # make codon table
+    codon_table = {}
+    for line in args.codons:
+        # kvpair = line.split()
+        # codon_table[kvpair[0]] = kvpair[1]
+        codon, letter = line.split()
+        codon_table[codon] = letter
+
+    # read codons
+    k = 3
+    seq = args.seq
+    for codon in [seq[i:i + k] for i in range(0, len(seq), k)]:
+        print(codon_table.get(codon.upper(), "-"), end='', file=args.outfile)
+
+    print(f'Output written to "{args.outfile.name}".')
 
 
 # --------------------------------------------------
